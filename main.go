f20fe5cb43e6dc893192773d08d18e24479ccea4
Dmitry Kirillov <dimakir1998@gmail.com>
2023-03-23 00:09:19 +0300

    Refactoring and add zerolog

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	log.Info().Msg("Starting server")
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to start server")
+	}
 }
