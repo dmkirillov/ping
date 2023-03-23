a75002f3cb0bd929549396c88e20ac9c913070cc
Dmitry Kirillov <dimakir1998@gmail.com>
2023-03-23 11:01:44 +0300

    add /health and /

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
+	"net/http"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "This is main page",
+		})
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "OK",
+		})
+	})
+
 	log.Info().Msg("Starting server")
 	err := r.Run(":8080")
 	if err != nil {
